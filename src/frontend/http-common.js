e4879d9a49a59a350395cfbaf5c83b59cdbf1f29
Arthur <xxx10r4@gmail.com>
2021-09-13 00:27:55 +0300

    heroku deploy

--- src/frontend/http-common.js
+++ src/frontend/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://currency-converter-huf2nok.herokuapp.com:8080/api",
     headers: {
         "Content-type": "application/json"
     }
